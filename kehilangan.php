0a3aec5b027d356dfb1056d3847753ee0061888a
yasinjauhari <22576005@stmik.jayakarta.ac.id>
2023-10-20 15:58:54 +0700

    Create kehilangan.php

---
+++ kehilangan.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <title>Buat Laporan Kehilangan</title>
+    </head>
+<body>
+        <h2>
+            Laporan Kehilangan
+        </h2>
+        <form action="tambahdata.php" method="post" name="form1">
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Nama :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="nama">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Merk :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="merk">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Plat :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="plat">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Telp :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="telp">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Email :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="email">
+            </div>
+            <div>
+                <input type="submit" name="Submit" style="background: blue; border-radius: 5px; padding: 5px; border: 2px solid blue; color: white;" value="Tambahkan">
+                <a href='index.php' style="border: 2px solid red; padding: 5px; border-radius: 5px; color: white; background-color: red; text-decoration: none;">Cancel</a>
+            </div>
+        </form>
+        <?php
+        // Chek apalah form sudah tersubmit, insert data dari tabel mtor
+        if(isset($_POST['Submit']))
+        {
+            $nama = $_POST['nama'];
+            $merk = $_POST['merk'];
+            $plat = $_POST['plat'];
+            $telp = $_POST['telp'];
+            $email = $_POST['email'];
+// Masukan file koneksi database
+            include("connect.php");
+// insert motor data ke tabel
+            $hasil = mysqli_query($connection, "INSERT INTO motor (nama,merk,plat,telp,email) VALUES('$nama', '$merk', '$plat', '$telp', '$email') ");
+// menampilkan pesan ketika berhasil di tambahkan
+            echo "Data Motor Berhasil DiTambahkan. <a href='index.php'>Data Motor</a>";
+        }
+        ?>
+    </body>
+</html>
