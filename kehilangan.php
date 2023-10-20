2e279ba811fe41892109792cdbc8ef8c11dce695
yasinjauhari <22576005@stmik.jayakarta.ac.id>
2023-10-20 19:54:55 +0700

    Update kehilangan.php

--- kehilangan.php
+++ kehilangan.php
@@ -6,7 +6,7 @@
         <h2>
             Laporan Kehilangan
         </h2>
-        <form action="tambahdata.php" method="post" name="form1">
+        <form action="kehilangan.php" method="post" name="form1">
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
@@ -45,7 +45,7 @@
                         Deskripsi :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="email">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="deskripsi">
             </div>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
@@ -53,7 +53,7 @@
                         Tgl Hilang :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="tgl_hilang">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="tgl_brg_ditemukan">
             </div>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
@@ -61,7 +61,23 @@
                         Lokasi :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="lokasi_hilang">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="lokasi_brg_ditemukan">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        status_barang :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="status_barang">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Foto Barang :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="foto_barang">
             </div>
             <div>
                 <input type="submit" name="Submit" style="background: blue; border-radius: 5px; padding: 5px; border: 2px solid blue; color: white;" value="Tambahkan">
@@ -72,17 +88,22 @@
         // Chek apalah form sudah tersubmit, insert data dari tabel mtor
         if(isset($_POST['Submit']))
         {
-            $nama = $_POST['nama'];
-            $merk = $_POST['merk'];
-            $plat = $_POST['plat'];
-            $telp = $_POST['telp'];
-            $email = $_POST['email'];
+            $id_member = $_POST['id_member'];
+            $jns_brg = $_POST['jns_brg'];
+            $nm_barang = $_POST['nm_barang'];
+            $brand = $_POST['brand'];
+            $deskripsi = $_POST['deskripsi'];
+            $tgl_brg_ditemukan = $_POST['tgl_brg_ditemukan'];
+            $lokasi_brg_ditemukan = $_POST['lokasi_brg_ditemukan'];
+            $status_barang = $_POST['status_barang'];
+            $foto_barang = $_POST['foto_barang'];
 // Masukan file koneksi database
             include("connect.php");
 // insert motor data ke tabel
-            $hasil = mysqli_query($connection, "INSERT INTO motor (nama,merk,plat,telp,email) VALUES('$nama', '$merk', '$plat', '$telp', '$email') ");
+            $hasil = mysqli_query($connection, "INSERT INTO barang_hilang (id_member,id_jns_brg,brand,deskripsi,tgl_brg_ditemukan,lokasi_brg_ditemukan,status_barang,foto_barang) 
+            VALUES('$id_member', '$jns_brg', '$nm_barang', '$brand', '$deskripsi','$tgl_brg_ditemukan','$lokasi_brg_ditemukan','$status_barang','$foto_barang') ");
 // menampilkan pesan ketika berhasil di tambahkan
-            echo "Data Motor Berhasil DiTambahkan. <a href='index.php'>Data Motor</a>";
+            echo "Data Barang Hilang Berhasil DiTambahkan. <a href='index.php'>Data Barang HIlang</a>";
         }
         ?>
     </body>
