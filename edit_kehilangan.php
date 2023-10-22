e5dbcd6bdb5163b834b7577afc6643e7827c10ad
yasinjauhari <22576005@stmik.jayakarta.ac.id>
2023-10-22 17:34:32 +0700

    Update edit_kehilangan.php

--- edit_kehilangan.php
+++ edit_kehilangan.php
@@ -13,9 +13,7 @@ if(isset($_POST['update']))
     $status_barang = $_POST['status_barang'];
     $foto_barang = $_POST['foto_barang'];
 // update barang_hilang data
-    $hasil = mysqli_query($connection, "UPDATE barang_hilang SET id_member='$id_member', id_jns_brg='$id_jns_brg', brand='$brand', 
-    deskripsi='$deskripsi', tgl_brg_ditemukan='$tgl_brg_ditemukan', lokasi_brg_ditemukan='$lokasi_brg_ditemukan', 
-    status_barang='$status_barang', foto_barang='$foto_barang' WHERE id_brg_hilang=$id_brg_hilang");
+    $hasil = mysqli_query($connection, "UPDATE barang_hilang SET id_member='$id_member', id_jns_brg='$id_jns_brg', brand='$brand', deskripsi='$deskripsi', tgl_brg_ditemukan='$tgl_brg_ditemukan', lokasi_brg_ditemukan='$lokasi_brg_ditemukan', status_barang='$status_barang', foto_barang='$foto_barang' WHERE id_brg_hilang='$id'");
     // lakukan redirect ke index untuk melihat hasil dari update data
     header("Location: index.php");
 }
@@ -26,17 +24,19 @@ $id = $_GET['id'];
 // Fetch barang_hilang data berdasarkan id
 $hasil = mysqli_query($connection, "SELECT * FROM
  barang_hilang WHERE id_brg_hilang=$id");
+
 while($data = mysqli_fetch_array($hasil))
  {
-    $id_member = ['id_member'];
-    $id_jns_brg = ['id_jns_brg'];
-    $brand = ['brand'];
-    $deskripsi = ['deskripsi'];
-    $tgl_brg_ditemukan = ['tgl_brg_ditemukan'];
-    $lokasi_brg_ditemukan = ['lokasi_brg_ditemukan'];
-    $status_barang = ['status_barang'];
-    $foto_barang = ['foto_barang'];
+    $id_member = $data['id_member'];
+    $id_jns_brg = $data['id_jns_brg'];
+    $brand = $data['brand'];
+    $deskripsi = $data['deskripsi'];
+    $tgl_brg_ditemukan = $data['tgl_brg_ditemukan'];
+    $lokasi_brg_ditemukan = $data['lokasi_brg_ditemukan'];
+    $status_barang = $data['status_barang'];
+    $foto_barang = $data['foto_barang'];
  }
+
 ?>
 <html>
     <head>
@@ -46,12 +46,12 @@ while($data = mysqli_fetch_array($hasil))
         <h2>
             Edit Data Pengguna
         </h2>
-        <form method="post" action="edit.php" name="formupload">
-            <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+        <form method="post" action="edit_kehilangan.php" name="formupload">
+            <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>"/>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
-                        Nama :
+                        ID Member :
                     </label>
                 </div>
                 <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="id_member" value="<?php echo $id_member; ?>">
@@ -59,34 +59,58 @@ while($data = mysqli_fetch_array($hasil))
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
-                        Merk :
+                        ID Jenis Barang :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="merk" value="<?php echo $merk; ?>">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="id_jns_brg" value="<?php echo $id_jns_brg; ?>">
             </div>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
-                        Plat :
+                        Brand :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="plat" value="<?php echo $plat; ?>">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="brand" value="<?php echo $brand; ?>">
             </div>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
-                        Telp :
+                        Deskripsi :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="telp" value="<?php echo $telp; ?>">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="deskripsi" value="<?php echo $deskripsi; ?>">
             </div>
             <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
                 <div style="margin-bottom: 5px;">
                     <label>
-                        Email :
+                        Tgl Barang ditemukan :
                     </label>
                 </div>
-                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="email" value="<?php echo $email; ?>">
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="tgl_brg_ditemukan" value="<?php echo $tgl_brg_ditemukan; ?>">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Lokasi Barang ditemukan :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="lokasi_brg_ditemukan" value="<?php echo $lokasi_brg_ditemukan; ?>">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Status Barang :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="status_barang" value="<?php echo $status_barang; ?>">
+            </div>
+            <div style="width: 100%; display: block; margin-top: 10px; margin-bottom: 10px;">
+                <div style="margin-bottom: 5px;">
+                    <label>
+                        Foto Barang :
+                    </label>
+                </div>
+                <input type="text" style="width: 100%; border: 2px solid black; border-radius: 5px;" name="foto_barang" value="<?php echo $foto_barang; ?>">
             </div>
             <div>
                 <input type="submit" name="update" style="background: blue; border-radius: 5px; padding: 5px; border: 2px solid blue; color: white;" value="Edit">
