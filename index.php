f131a83e70666946652e5e8b5eb27a45e4bd76c8
arthasasta <116079643+arthasasta@users.noreply.github.com>
2023-10-25 21:32:57 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // buat koneksi database dengan connect file
 include("connect.php");
-// fetch semua data barang_hilang dari database
-$hasil = mysqli_query($connection, "SELECT * FROM barang_hilang ORDER by id_brg_hilang DESC");
+// fetch semua data motor dari database
+$hasil = mysqli_query($connection, "SELECT * FROM orang_hilang ORDER by id_osrg_hilang DESC");
 ?>
 <html>
     <head>
@@ -19,21 +19,22 @@ $hasil = mysqli_query($connection, "SELECT * FROM barang_hilang ORDER by id_brg_
         padding: 5px;
         float: right;">
             <a href="kehilangan.php" style="text-decoration: none; color: black;">
-                Tambah Data Barang Hilang
+                Tambah Data Orang Hilang
             </a>
         </div>
         <table style="width: 100%;" border="1">
             <thead>
                 <tr>
+                    <th>id_org_hilang</th>
                     <th>id_member</th>
-                    <th>id_jns_brg</th>
-                    <th>brand</th>
-                    <th>deskripsi</th>
-                    <th>tgl_brg_ditemukan</th>
-                    <th>lokasi_brg_ditemukan</th>
-                    <th>status_barang</th>
-                    <th>foto_barang</th>
-                    <th>Action</th>
+                    <th>id_jns_kelamin</th>
+                    <th>nama_orang_hilang</th>
+                    <th>tgl_lahir</th>
+                    <th>deskripsi_fisik</th>
+                    <th>tgl_ditemukan</th>
+                    <th>lokasi_org_ditemukan</th>
+                    <th>status_orang</th>
+                    <th>foto_orang</th>
                 </tr>
             </thead>
             <tbody>
@@ -41,14 +42,16 @@ $hasil = mysqli_query($connection, "SELECT * FROM barang_hilang ORDER by id_brg_
                 while($barang_hilang = mysqli_fetch_array($hasil))
                 {
                     echo "<tr>";
+                    echo "<td>".$barang_hilang['id_org_hilang']."</td>";
                     echo "<td>".$barang_hilang['id_member']."</td>";
-                    echo "<td>".$barang_hilang['id_jns_brg']."</td>";
-                    echo "<td>".$barang_hilang['brand']."</td>";
-                    echo "<td>".$barang_hilang['deskripsi']."</td>";
-                    echo "<td>".$barang_hilang['tgl_brg_ditemukan']."</td>";
-                    echo "<td>".$barang_hilang['lokasi_brg_ditemukan']."</td>";
-                    echo "<td>".$barang_hilang['status_barang']."</td>";
-                    echo "<td>".$barang_hilang['foto_barang']."</td>";
+                    echo "<td>".$barang_hilang['id_jns_kelamin']."</td>";
+                    echo "<td>".$barang_hilang['nama_orang_hilang']."</td>";
+                    echo "<td>".$barang_hilang['tgl_lahir']."</td>";
+                    echo "<td>".$barang_hilang['deskripsi_fisik']."</td>";
+                    echo "<td>".$barang_hilang['tgl_ditemukan']."</td>";
+                    echo "<td>".$barang_hilang['lokasi_org_ditemukan']."</td>";
+                    echo "<td>".$barang_hilang['status_orang']."</td>";
+                    echo "<td>".$barang_hilang['foto_orang']."</td>";
                     echo "<td>
                     <a href='edit_kehilangan.php?id=$barang_hilang[id_brg_hilang]'>Edit</a> | <a href='delete_kehilangan.php?id=$barang_hilang[id_brg_hilang]'>Delete</a></td>";
                     echo "</tr>";
@@ -58,46 +61,3 @@ $hasil = mysqli_query($connection, "SELECT * FROM barang_hilang ORDER by id_brg_
         </table>
     </body>
 </html>
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css"/>
-
-    <title>WELCOME</title>
-</head>
-<body>
-    
-
-<div class="page1">
-       <p class="apa-yang-kamu-butuhkan">APA YANG KAMU BUTUHKAN?</p>
-    
-
-    <div class="group1"> 
-            <div class="gambar1" onclick="menemukan_barang()"></div>
-            <div class="gambar2" onclick="kehilangan_barang()"> </div>
-    </div>
-
-
-    <div class="group2">
-        
-            <p class="menemukan-barang" onclick="menemukan_barang()">Saya Menemukan Barang</p>
-            <p class="kehilangan-barang" onclick="kehilangan_barang()"> Saya Kehilangan Barang</p>
-        
-    </div>
-    
-</div>
-
-<script src="script.js"></script>
-
-
-</body>
-
-</html>
-*/
-
-?> -->
